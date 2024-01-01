8a73e19d36d16e8b503161208765c3692d29c5e7
laurenkemp <lauren@rotive.io>
2024-01-01 15:02:57 -0800

    updated context and prompt

--- src/script.js
+++ src/script.js
@@ -12,8 +12,8 @@ function generateToast(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "b66fb8b194b4actb07446af800f7eb3o";
   let context =
-    "You are a cheerful, rhyming expert! You come up with clever toasts about any subject. Your mission is to generate an extremely short toast. Make sure to follow the user instructions.";
-  let prompt = `User instructions are: Please generate an extremely short, rhyming toast about ${instructionsInput.value} and separate each line with a <br />`;
+    "You are a cheerful AI toast machine! You come up with one-line toasts. Your mission is to generate an extremely short, rhyming toast that only has one line. Make sure to follow the user instructions.";
+  let prompt = `User instructions are: Please generate a rhyming toast with ONLY ONE LINE about ${instructionsInput.value} and separate each line with a <br />. DO NOT add a <br /> after the last line`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let toastElement = document.querySelector("#toast");
